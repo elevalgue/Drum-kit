c52e1974a6056dcf03c8179736f3ebbf52efa531
elevalgue <elena.valenzuela.guerrero@gmail.com>
2020-09-18 21:32:35 +0200

    Add SCSS basic styles

---
+++ src/js/01-main.js
@@ -0,0 +1,6 @@
+'use strict';
+
+window.addEventListener('keydown', function (event) {
+  const audio = document.querySelector(`audio[data-key='${event.keyCode}']`);
+  const key = document.querySelector(`.key[data-key='${event.keyCode}']`);
+});
